eaf3339db3511f92726567ddde4e62d0cd7e646b
mrwanbot <mrwanada2024@gmail.com>
2026-07-03 22:29:45 +0100

    Remove CORS middleware and update response message

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-    res.send("bQTtPanel API يعمل بنجاح 🚀");
+  res.send("bQTtPanel API يعمل ✅");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log("Server Started");
 });
